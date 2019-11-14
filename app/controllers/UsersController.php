bdd2fce336718c077fe993a866c45fbbe5dc9f73
markoreljin <reljinmarko@gmail.com>
2019-11-14 21:20:05 +0100

    - Barebone registration validation finished

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controllers;
 
+use App\Controllers\HttpExceptions\Http400Exception;
 use App\Models\Users;
 use App\Services\UsersService;
+use App\Helpers\Validators\RegistrationValidator;
 
 /**
  * Operations with Users: CRUD
@@ -15,21 +17,6 @@ class UsersController extends AbstractController
         
     }
 
-    /*
-    Registracija obuhvata unos
-    email adrese, lozinke, imena, prezimena, adrese prebivališta, grada, države,
-    broja telefona i jedinstveni broj osiguranika. Lozinka se unosi u dva polja da bi se
-    otežalo pravljenje grešaka prilikom odabira nove lozinke. Nakon popunjavanja
-    forme za registraciju, zahtev se šalje administratoru kliničkog centra na reviziju.
-    Zahtev za registracijom administrator može da potvrdi ili odbije. Nakon
-    odobravanja zahteva za registracijom, na datu email adresu se šalje link za
-    aktivaciju korisnika. Korisnik ne može da se prijavi na aplikaciju dok se njegov
-    nalog ne aktivira posećivanjem linka koji je dobio u emailu. Ukoliko je zahtev
-    odbijen, korisniku se na email adresu šalje poruka da je zahtev odbijen uz kratku
-    poruku gde administrator navodi razlog odbijanja zahteva.
-    Napomena: potrebno je obezbediti bilo kakav mehanizam za autentifikaciju i
-    autorizaciju korisnika na serverskoj strani.
-    */
     public function registerAction()
     {
         $errors = [];
@@ -44,6 +31,18 @@ class UsersController extends AbstractController
             $country = $this->request->getPost('country');
             $phone = $this->request->get('phone');
 
+            $validation = new RegistrationValidator();
+            $messages = $validation->validate($this->request->getPost());
+
+            if (count($messages)) {
+                foreach ($messages as $message) {
+                    $errors[] = [
+                        'code' => $message->getCode(),
+                        'message' => $message->getMessage()];
+                }
+                $exception = new Http400Exception(_('Invalid registration parameters'), self::ERROR_BAD_REQUEST);
+                throw $exception->addErrorDetails($errors);
+            }
             UsersService::register($id, $firstName, $lastName, $email, $password, 
                                               $address, $city, $country, $phone);
 
